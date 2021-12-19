e2e6e09fe64bd85ef1272a54364c4d50237753ad
Fredrik Lindin <fredriklindin@gmail.com>
2021-12-19 16:51:42 +0100

    Add plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +10,10 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>yipp</title>
       </Head>
+      <Script
+        data-domain="yipp.dev"
+        src="https://plausible.io/js/plausible.js"
+      />
       <Component {...pageProps} />
     </Layout>
   );
