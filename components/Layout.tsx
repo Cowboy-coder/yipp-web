7caa2af071aa7f10d9f450374caf5e5e477874ba
Fredrik Lindin <fredriklindin@gmail.com>
2021-12-19 14:18:26 +0100

    Minor fixes

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,27 +1,20 @@
 const Layout: React.FC = ({ children }) => {
   return (
-    <div className="text-amber-100 min-h-screen pb-12 px-2">
-      <div className="text-amber-100 font-['Helvetica'] tracking-widest py-2 h-54px leading-[54px] max-w-[800px] mx-auto text-[34px]">
+    <div className="min-h-screen pb-12 px-2">
+      <div className="tracking-widest py-2 h-54px leading-[54px] max-w-[800px] mx-auto text-[34px]">
         yipp
       </div>
       <div className="w-full mx-auto grid max-w-[800px]">
-        <p className="text-md pb-4 max-w-xl">
-          yipp is a schema format for building HTTP REST servers and clients,
-          borrowing heavily from the GQL syntax.
+        <p className="text-md pb-4">
+          A schema format for building JSON APIs, heavily inspired by GQL.
         </p>
         <p className="text-sm pb-8 max-w-lg">
           Check out how it can be used to generate a{" "}
-          <a
-            className="text-sky-500"
-            href="https://github.com/Cowboy-coder/yipp/tree/master/src/examples/fastify"
-          >
+          <a href="https://github.com/Cowboy-coder/yipp/tree/master/src/examples/fastify">
             fully typed, schema enforced fastify plugin
           </a>{" "}
           or a{" "}
-          <a
-            className="text-sky-500"
-            href="https://github.com/Cowboy-coder/yipp/tree/master/src/examples/axios-client"
-          >
+          <a href="https://github.com/Cowboy-coder/yipp/tree/master/src/examples/axios-client">
             fully typed axios client
           </a>
           . Since the yipp specification is relatively small it's meant to be
@@ -29,15 +22,9 @@ const Layout: React.FC = ({ children }) => {
           programming language, server side framework or HTTP client.
         </p>
         {children}
-        <p className="font-mono text-sm text-amber-100 pt-6">
-          Check out{" "}
-          <a
-            className="text-sky-500"
-            href="https://github.com/Cowboy-coder/yipp"
-          >
-            yipp
-          </a>{" "}
-          on github.
+        <p className="font-mono text-sm pt-6">
+          Check out <a href="https://github.com/Cowboy-coder/yipp">yipp</a> on
+          github.
         </p>
       </div>
     </div>
